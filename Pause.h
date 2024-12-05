a5b78ef13d3d5cbda0b0389a01cc882e6b454c49
Azriella247 <152945342+Azriella247@users.noreply.github.com>
2024-12-05 12:53:04 -0600

    Add files via upload

---
+++ Pause.h
@@ -0,0 +1,20 @@
+/*
+* Author: Nykayla Steadmon
+* File Name: Pause.h
+* File Description:
+* Date Created: 12/4/2024
+* Date Last Modified: 12/4/2024
+*/
+
+#ifndef Pause_h
+#define Pause_h
+
+#include "SDL_Plotter.h"
+using namespace std;
+
+void drawPauseMenu(SDL_Plotter &g);
+
+bool handlePauseMenu(SDL_Plotter &g);
+
+
+#endif /* Pause_h */
